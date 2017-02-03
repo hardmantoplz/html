27674ad5cf31baff1219f586eca7ce5c426ba087
jeff <jmw6886@yahoo.com>
2017-02-03 10:32:08 -0800

    Add files via upload

---
+++ some.js
@@ -0,0 +1,13 @@
+/*
+function ()init{
+	var h1tag = document.getElementsByTagName("h1")
+	h1tags [0] onclick = changeColor;
+}
+function changeColor(){
+	this.innerHTML = "click again";
+	var randomcolor = '#'+Math.floor(Math.random()*16777215).toString(16);
+	this.style.color= randomcolor;
+		
+	}
+
+onload init
